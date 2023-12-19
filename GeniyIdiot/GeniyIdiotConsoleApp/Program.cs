aec1d395941cf4fcf610bc54ee83c044b89b9597
By-pa <by.pa28@gmail.com>
2023-12-19 21:25:28 +0300

    Исправил не дочеты

--- GeniyIdiot/GeniyIdiotConsoleApp/Program.cs
+++ GeniyIdiot/GeniyIdiotConsoleApp/Program.cs
@@ -6,10 +6,6 @@
 		{
 			while (true)
 			{
-
-
-
-
 				Console.WriteLine("Здравствуйте! Как Тебя зовут?)");
 				string userName = Console.ReadLine();
 
@@ -74,8 +70,9 @@
 			int percentRightAnswers = countRightAnswers * 100 / countQuestions;
 
 			return diagnoses[percentRightAnswers / 20];
+		}
 
-		private static int GetUserAnswer()
+		static int GetUserAnswer()
 		{
 			while (true)
 			{
@@ -83,75 +80,75 @@
 				{
 					return Convert.ToInt32(Console.ReadLine());
 				}
-				catch(FormatException)
+				catch (FormatException)
 				{
-                    Console.WriteLine("Введите число!");
-                } 
-				catch(OverflowException)
+					Console.WriteLine("Введите число!");
+				}
+				catch (OverflowException)
 				{
-                    Console.WriteLine("Вы ввели слишком большое число!");
-                }
-				
-				
+					Console.WriteLine("Вы ввели слишком большое число!");
+				}
+
+
 
 			}
 		}
 
-		static bool GetUserChoise(string message)
-		{
-			while (true)
+			static bool GetUserChoise(string message)
 			{
-
-
-				Console.WriteLine(message + " Введите Да или Нет");
-				string userInput = Console.ReadLine();
-
-				if (userInput.ToLower() == "нет")
+				while (true)
 				{
-					return false;
-				}
-				if (userInput.ToLower() == "да")
-				{
-					return true;
+
+
+					Console.WriteLine(message + " Введите Да или Нет");
+					string userInput = Console.ReadLine();
+
+					if (userInput.ToLower() == "нет")
+					{
+						return false;
+					}
+					if (userInput.ToLower() == "да")
+					{
+						return true;
+					}
 				}
 			}
-		}
-		static string[] GetQuestions(int countQuestions)
-		{
-			string[] questions = new string[countQuestions];
-			questions[0] = "Сколько будет два плюс два умноженное на два?";
-			questions[1] = "Бревно нужно распилить на 10 частей, сколько надо сделать распилов?";
-			questions[2] = "На двух руках 10 пальцев. Сколько пальцев на 5 руках?";
-			questions[3] = "Укол делают каждыйе пол часа, сколько нужно минут для трех уколов?";
-			questions[4] = "Пять свечей горело, две потухли. Сколько свечей осталось?";
+			static string[] GetQuestions(int countQuestions)
+			{
+				string[] questions = new string[countQuestions];
+				questions[0] = "Сколько будет два плюс два умноженное на два?";
+				questions[1] = "Бревно нужно распилить на 10 частей, сколько надо сделать распилов?";
+				questions[2] = "На двух руках 10 пальцев. Сколько пальцев на 5 руках?";
+				questions[3] = "Укол делают каждыйе пол часа, сколько нужно минут для трех уколов?";
+				questions[4] = "Пять свечей горело, две потухли. Сколько свечей осталось?";
 
-			return questions;
-		}
+				return questions;
+			}
 
-	
-		static string[] GetDiagnoses()
-		{
-			string[] diagnoses = new string[6];
-			diagnoses[0] = "Идиот";
-			diagnoses[1] = "Кретин";
-			diagnoses[2] = "Дурак";
-			diagnoses[3] = "Нормальный";
-			diagnoses[4] = "Талант";
-			diagnoses[5] = "Гений";
 
-			return diagnoses;
-		}
+			static string[] GetDiagnoses()
+			{
+				string[] diagnoses = new string[6];
+				diagnoses[0] = "Идиот";
+				diagnoses[1] = "Кретин";
+				diagnoses[2] = "Дурак";
+				diagnoses[3] = "Нормальный";
+				diagnoses[4] = "Талант";
+				diagnoses[5] = "Гений";
 
-		static int[] GetAnswer(int countQuestions)
-		{
-			int[] answers = new int[countQuestions];
-			answers[0] = 6;
-			answers[1] = 9;
-			answers[2] = 25;
-			answers[3] = 60;
-			answers[4] = 2;
-			return answers;
+				return diagnoses;
+			}
+
+			static int[] GetAnswer(int countQuestions)
+			{
+				int[] answers = new int[countQuestions];
+				answers[0] = 6;
+				answers[1] = 9;
+				answers[2] = 25;
+				answers[3] = 60;
+				answers[4] = 2;
+				return answers;
+			}
 		}
 		
-	}
 }
